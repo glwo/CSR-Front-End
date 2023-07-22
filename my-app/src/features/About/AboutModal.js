30bbf381521008c77edf894037339a0d94bc87cc
glen <glenwojnar@yahoo.com>
2023-07-22 19:39:50 -0400

    finished styling about

--- my-app/src/features/About/AboutModal.js
+++ my-app/src/features/About/AboutModal.js
@@ -5,9 +5,21 @@ const AboutModal = ({ onClose }) => {
   return (
     <div className="about-modal-overlay">
       <div className="about-modal-content">
-        <h2>About Customer Service Portal</h2>
+        <h2>About the Customer Service Portal</h2>
         <p>
-          // Describe the site's functions here...
+          The Customer Service Portal is a web application designed to provide excellent customer service to our users. It allows our support staff to manage user accounts, subscriptions, and purchase history effectively.
+        </p>
+        <h3>
+          Key Features:
+        </h3>
+        <ol>
+          <li>Account Management: View and edit user account details, such as name, email, and phone number.</li>
+          <li>Subscription Management: Manage user subscriptions, including adding, removing, and transferring subscriptions between users or vehicles.</li>
+          <li>Purchase History: Access and review the purchase history of users to provide better support.</li>
+          <li>Search Functionality: Easily search and find users based on their names, email addresses, or phone numbers.</li>
+        </ol>
+        <p>
+          The Customer Service Portal is a powerful tool that streamlines customer support operations and ensures a seamless experience for our valued users.
         </p>
         <button onClick={onClose}>Close</button>
       </div>
@@ -16,3 +28,4 @@ const AboutModal = ({ onClose }) => {
 };
 
 export default AboutModal;
+
